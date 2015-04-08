5edde41a11c265ab94fbfd4b7d4e0bb348655b16
Desert Storm <PetrifiedLasagna@users.noreply.github.com>
2015-04-07 22:18:08 -0500

    Create utils.h

---
+++ source/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/* This comment must not be removed
+This  File was written by PetrifiedLasagna and is from the following Github repository
+https://github.com/PetrifiedLasagna/random-number-generator-and-visualizer
+
+these files should come with a file explaining their usage/license.
+*/
+
+void genrseed();
+unsigned long rnd();
+
+#endif // UTILS_H
